e28c66bf9651cb876162749af29cf9e8b316bf44
mwaniqi <mwaniqi@gmail.com>
2018-07-22 23:28:24 -0700

    add countdown control buttons; modify duration button handlers

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class Display extends Component {
     var displayStyles = {
       width: 100,
       height: 50,
-      border: '1px solid #b4b3b3',
+      border: '1px solid rgb(129, 87, 87)',
       borderRadius: 10,
-      margin: '0 auto 36px auto',
-      padding: 16,
+      margin: '64px auto',
+      padding: 32,
+      fontSize: '72px',
+      fontWeight: 'bold',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center'
@@ -25,7 +27,12 @@ class Display extends Component {
 class Button extends Component {
   render() {
     var props = this.props
-    var action = props.action === 'down' ? '-' : '+'
+    var action = props.action === 'down' ? '-'
+      : props.action === 'up' ? '+'
+      : props.action === 'start' ? 'Start'
+      : props.action === 'pause' ? 'Pause'
+      : 'Reset'
+
     var handler = (props.id === 'reduceSession') ? props.reduceSession
       : (props.id === 'addSession') ? props.addSession
       : (props.id === 'reduceBreak') ? props.reduceBreak
@@ -49,10 +56,13 @@ class App extends Component {
 
   toggleActive(e) {
     var labels = Array.from(document.querySelectorAll('.label'))
+    var modes = Array.from(document.querySelectorAll('.mode'))
 
     labels.forEach(function(label) {
-      label.classList.remove('active')
-      e.target.classList.add('active')
+      modes.forEach((mode)=> {
+        mode.classList.remove('active')
+      })
+      e.target.parentElement.classList.add('active')
     })
   }
 
@@ -108,26 +118,23 @@ class App extends Component {
       addBreak: this.increaseBreak.bind(this),
       reduceBreak: this.decreaseBreak.bind(this)
     }
-   
+
     return (
       <main className="App">
         <header className="App-header">
           <h1 className="App-title">Pomodoro Clock</h1>
-          <p className="App-intro">Set Pomodoro and Break Duration</p>
         </header>
 
-        <Display {...timer} />
-
-        <section className='mode'>
-          <div id='pomodoroLength'>
-            <h3 ref='pomodoroLabel' className='label active'
+        <section id='mode'>
+          <div id='pomodoroLength' className='mode active'>
+            <h3 ref='pomodoroLabel' className='label'
               onClick={this.toggleActive.bind(this)}>Pomodoro</h3>
             <Button action='down' {...plusMinus} id='reduceSession'/>
             <span>{this.state.session}</span>
             <Button action='up' {...plusMinus} id='addSession'/>
           </div>
 
-          <div id='breakLength'>
+          <div id='breakLength' className='mode'>
             <h3 ref='pomodoroLabel' className='label'
               onClick={this.toggleActive.bind(this)}>Break</h3>
             <Button action='down' {...plusMinus} id='reduceBreak'/>
@@ -135,6 +142,14 @@ class App extends Component {
             <Button action='up' {...plusMinus} id='addBreak'/>
           </div>
         </section>
+
+        <Display {...timer} />
+
+        <section id='controls'>
+          <Button action='start'/>
+          <Button action='pause'/>
+          <Button action='reset'/>
+        </section>        
       </main>
     )
   }
